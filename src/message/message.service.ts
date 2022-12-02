2b48f7c8d621daccabdb1732ee5e207acc2aaee2
nike0214 <nike0214@label.foundation>
2022-12-03 03:21:59 +0900

    fix: S3 configuration

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -14,10 +14,10 @@ import AWS from 'aws-sdk';
 
 const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET;
 const s3 = new AWS.S3({
-  signatureVersion: 'v4',
-  endpoint: process.env.AWS_ENDPOINT,
-  region: process.env.AWS_S3_REGION,
-  useAccelerateEndpoint: true,
+  region: 'ap-northeast-2',
+  // endpoint: process.env.AWS_ENDPOINT,
+  // region: process.env.AWS_S3_REGION,
+  // useAccelerateEndpoint: true,
 });
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -124,10 +124,10 @@ export class MessageService {
     try {
       const params = {
         Bucket: 'dates-temp',
-        Key: fileName,
+        Key: `${fileName}.jpg`,
         Expires: 3600,
-        ContentType: 'image/jpeg',
-        ACL: 'public-read',
+        // ContentType: 'image/png',
+        // ACL: 'public-read',
       };
       const s3Url = await s3.getSignedUrlPromise('putObject', params);
 
